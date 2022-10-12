8df30ec3164c58095f3cf950ee2970f08b94f96a
pentad0 <115344116+pentad0@users.noreply.github.com>
2022-10-12 23:25:05 +0900

    Update export_all_characteres.bat

--- export_all_characteres/export_all_characteres.bat
+++ export_all_characteres/export_all_characteres.bat
@@ -1,6 +1,6 @@
-rem set targetDir="./target_text"
+set targetDir="./target_text"
 rem set targetDir="../replace_simplified_kanji/target_text"
-set targetDir="../replace_simplified_kanji/output_text"
+rem set targetDir="../replace_simplified_kanji/output_text"
 
 py export_all_characteres.py %targetDir% ./export/all_characteres.txt
 rem py export_all_characteres.py %targetDir% ./export/all_characteres.txt --sep_lf
